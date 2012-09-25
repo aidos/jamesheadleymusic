1e4192883638a4713e06b3af5c5638a9279b58d3
Aidan Kane <aidankane@gmail.com>
2012-09-25 22:24:17 +0000

    adding content and slider behaviour

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -13,7 +13,48 @@ require.config({
 // Load our app
 define(['jquery', 'lazyload', 'libs/ios-zoom-fix'], function() {
 
+  // portfolio
+  $('.portfolio').each(function(){
+    var portfolio = $(this);
+    var elms = portfolio.find('li');
+    var current = 0;
 
+    // tracking of max positions
+    var shell_width = 0;
+
+    do_layout = function() {
+      // need to shuffle stuff around to keep the fluid layout
+      shell_width = portfolio.parent().width();
+      elms.width(shell_width);
+      portfolio.css({marginLeft: -current * shell_width});
+    }
+
+    go_next = function(e) {
+      e.preventDefault();
+      current++;
+      if (current > elms.length - 1) {
+        current = elms.length - 1;
+      }
+      portfolio.animate({marginLeft: -current * shell_width});
+    }
+    
+    go_prev = function(e) {
+      e.preventDefault();
+      current--;
+      if (current < 0) {
+        current = 0;
+      }
+      portfolio.animate({marginLeft: -current * shell_width});
+    }
+
+    $(window).resize(do_layout);
+    do_layout();
+
+
+    $('.portfolio-next').click(go_next);
+    $('.portfolio-prev').click(go_prev);
+
+  });
 
 });
 
