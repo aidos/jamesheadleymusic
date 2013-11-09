778991f83f802b07f9f6c51301d2bed2d4a019af
Aidan Kane <aidankane@gmail.com>
2013-11-09 21:30:30 +0000

    soundcloud popovers

--- app/site.py
+++ app/site.py
@@ -2,17 +2,31 @@ from flask import Flask, request, jsonify, render_template, g
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route("/soundcloud/apprentice.html", methods=['GET'])
 def apprentice():
     return render_template('apprentice.html')
 
+
+@app.route("/soundcloud/shelf-life.html", methods=['GET'])
+def shelf_life():
+    return render_template('shelf-life.html')
+
+
+@app.route("/soundcloud/jennings.html", methods=['GET'])
+def jennings():
+    return render_template('jennings.html')
+
+
 @app.route("/soundcloud/tower.html", methods=['GET'])
 def tower():
     return render_template('tower.html')
+
 
 if __name__ == "__main__":
     app.debug = True
