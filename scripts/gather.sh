1e4c49d226141ec5622643f05eb7b3e8a2e30fdd
Aidan Kane <aidankane@gmail.com>
2014-08-01 12:35:10 +0000

    change of servers

--- scripts/gather.sh
+++ scripts/gather.sh
@@ -2,8 +2,8 @@
 
 rm -rdf ./dist
 
-wget --recursive http://192.168.56.101:5012
-mv "192.168.56.101:5012" dist
+wget --recursive http://192.168.33.10:5012
+mv "192.168.33.10:5012" dist
 cp -r ../app/static/js/* ./dist/static/js/
 
 mv dist/soundcloud/apprentice.html\?iframe\=true dist/soundcloud/apprentice.html
